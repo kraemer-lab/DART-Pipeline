7492d6d9a7c9bc7a8e47cd21e5681a615d063ca1
prathyush22 <113349869+prathyush22@users.noreply.github.com>
2023-10-31 14:51:16 +0000

    rwi processing update 1

--- codes/relative_wealth_index_processing_and_plotting.py
+++ codes/relative_wealth_index_processing_and_plotting.py
@@ -18,8 +18,10 @@ df = pd.read_csv(input_file)
 gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
 
 fig, ax = plt.subplots(figsize=(15,12))
-gdf.plot(ax=ax, column = 'rwi', marker = 'o', markersize=1,legend=True)
+gdf.plot(ax=ax, column = 'rwi', marker = 'o', markersize=1, label = 'RWI score', legend=True)
 contextily.add_basemap(ax,crs={'init':'epsg:4326'},source=contextily.providers.OpenStreetMap.Mapnik)
+plt.title('Relative Wealth Index scores of locations in Vietnam')
+plt.legend()
 plt.savefig(plot_path, dpi=600)
  
 
