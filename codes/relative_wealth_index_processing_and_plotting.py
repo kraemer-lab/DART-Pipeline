78433dc02d27ef29413f5494bd73f297e68fa4ea
prathyush22 <113349869+prathyush22@users.noreply.github.com>
2023-10-31 14:42:01 +0000

    relative wealth index processing

---
+++ codes/relative_wealth_index_processing_and_plotting.py
@@ -0,0 +1,26 @@
+#Author: Prathyush Sambaturu
+#Purpose: To process the relative wealth index file (.csv format) and plot the rwi scores for each location given by latitude and longitude.
+
+#Load the necessary packages
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from shapely.geometry import Point
+import contextily
+import sys
+
+#Assign values to variables using arguments from command line
+input_file = sys.argv[1]
+plot_path = sys.argv[2]
+
+#Read the input file as a dataframe and convert it to a geodataframe
+df = pd.read_csv(input_file)
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
+
+fig, ax = plt.subplots(figsize=(15,12))
+gdf.plot(ax=ax, column = 'rwi', marker = 'o', markersize=1,legend=True)
+contextily.add_basemap(ax,crs={'init':'epsg:4326'},source=contextily.providers.OpenStreetMap.Mapnik)
+plt.savefig(plot_path, dpi=600)
+ 
+
+
