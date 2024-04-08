fd3ccdd3c04621caffed063b380dc868c492a55c
Rowan Nicholls <rowan.nicholls@dtc.ox.ac.uk>
2024-04-08 16:58:24 +0100

    Updated tests

--- A Collate Data/test_collate_data.py
+++ A Collate Data/test_collate_data.py
@@ -19,9 +19,10 @@ from pathlib import Path
 import utils
 from collate_data import \
     get_credentials, \
+    download_file, \
+    download_files, \
     walk, \
     download_gadm_data, \
-    download_file, \
     unpack_file, \
     download_meteorological_data, \
     download_aphrodite_temperature_data, \
@@ -39,7 +40,7 @@ from collate_data import \
 class TestCases(unittest.TestCase):
 
     @patch('collate_data.open')
-    def test_get_credentials_default(self, mock_open):
+    def test_get_credentials(self, mock_open):
         # Mock the contents of the credentials file
         file = '''{
             "Example metric": {
@@ -49,31 +50,122 @@ class TestCases(unittest.TestCase):
         }'''
         mock_open.return_value.__enter__.return_value.read.return_value = file
 
-        # Call the function being tested
+        # Test the functionality when the file is in the default location
         username, password = get_credentials('Example metric')
-
-        # Perform the tests
         self.assertEqual(username, 'Example username')
         self.assertEqual(password, 'Example password')
 
-    @patch('collate_data.open')
-    def test_get_credentials_moved(self, mock_open):
-        # Mock the contents of the credentials file
-        file = '''{
-            "Example metric": {
-                "username": "Example username",
-                "password": "Example password"
-            }
-        }'''
-        mock_open.return_value.__enter__.return_value.read.return_value = file
-
-        # Call the function being tested
+        # Test the functionality when the file is in an unexpected location
         path = Path('alternative/path/to/example_credentials.json')
         uname, password = get_credentials('Example metric', credentials=path)
-
-        # Perform the tests
         self.assertEqual(uname, 'Example username')
         self.assertEqual(password, 'Example password')
+
+    def test_download_file(self):
+        #
+        # Test a successful download
+        #
+        url = 'https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_VNM_0.json'
+        out_dir = Path('tests')
+        out_dir.mkdir(parents=True, exist_ok=True)
+        path = Path(out_dir, 'gadm41_VNM_0.json')
+        succeded = download_file(url, path)
+        # Check if the file has been created
+        expected = True
+        actual = path.exists()
+        self.assertEqual(expected, actual)
+        # Check if the value returned from the function is correct
+        expected = True
+        actual = succeded
+        self.assertEqual(expected, actual)
+        # Tear down
+        path.unlink()
+        Path('tests/').rmdir()
+
+        #
+        # Test an unsuccessful download
+        #
+        # This will fail because a password is needed
+        url = 'http://aphrodite.st.hirosaki-u.ac.jp/product/' + \
+            'APHRO_V1808_TEMP/APHRO_MA/005deg/' + \
+            'APHRO_MA_TAVE_CLM_005deg_V1808.ctl.gz'
+        out_dir = Path('tests/product/APHRO_V1808_TEMP/APHRO_MA/005deg')
+        out_dir.mkdir(parents=True, exist_ok=True)
+        path = Path(out_dir, 'APHRO_MA_TAVE_CLM_005deg_V1808.ctl.gz')
+        succeded = download_file(url, path)
+        # Check if the file has been created
+        expected = False
+        actual = path.exists()
+        self.assertEqual(expected, actual)
+        # Check if the value returned from the function is correct
+        expected = False
+        actual = succeded
+        self.assertEqual(expected, actual)
+        # Tear down
+        Path('tests/product/APHRO_V1808_TEMP/APHRO_MA/005deg').rmdir()
+        Path('tests/product/APHRO_V1808_TEMP/APHRO_MA').rmdir()
+        Path('tests/product/APHRO_V1808_TEMP').rmdir()
+        Path('tests/product').rmdir()
+        Path('tests/').rmdir()
+
+    def test_download_files(self):
+        # Setup
+        out_dir = Path('tests')
+        out_dir.mkdir(parents=True, exist_ok=True)
+
+        #
+        # Test successful downloads
+        #
+        base_url = 'https://geodata.ucdavis.edu'
+        relative_url = 'gadm/gadm4.1/json'
+        files = ['gadm41_VNM_0.json', 'gadm41_VNM_1.json']
+        successes = download_files(
+            base_url, relative_url, files, only_one=False, dry_run=False,
+            out_dir=out_dir
+        )
+        # Check if the value returned from the function is correct
+        expected = [True, True]
+        actual = successes
+        self.assertEqual(expected, actual)
+        # Check if the files exist
+        expected = True
+        for file in files:
+            path = Path(out_dir, relative_url, file)
+            actual = path.exists()
+            self.assertEqual(expected, actual)
+            # Tear down
+            path.unlink()
+        # Tear down
+        Path('tests/gadm/gadm4.1/json/').rmdir()
+        Path('tests/gadm/gadm4.1/').rmdir()
+        Path('tests/gadm/').rmdir()
+
+        #
+        # Test an unsuccessful download
+        #
+        base_url = 'https://geodata.ucdavis.edu'
+        relative_url = 'gadm/gadm4.1/json'
+        files = ['gadm41_VNM_0.json', 'gadm41_VNM_4.json']
+        successes = download_files(
+            base_url, relative_url, files, only_one=False, dry_run=False,
+            out_dir=out_dir
+        )
+        # Check if the value returned from the function is correct
+        expected = [True, False]
+        actual = successes
+        self.assertEqual(expected, actual)
+        # Check if the files exist/do not exist
+        expected = [True, False]
+        for i, file in enumerate(files):
+            path = Path(out_dir, relative_url, file)
+            actual = path.exists()
+            self.assertEqual(expected[i], actual)
+        # Tear down
+        Path('tests/gadm/gadm4.1/json/gadm41_VNM_0.json').unlink()
+        Path('tests/gadm/gadm4.1/json/').rmdir()
+        Path('tests/gadm/gadm4.1/').rmdir()
+        Path('tests/gadm/').rmdir()
+        Path('tests/').rmdir()
 
     def test_walk(self):
         base_url = 'https://data.chc.ucsb.edu'
@@ -120,56 +212,6 @@ class TestCases(unittest.TestCase):
         path.unlink()
         Path('tests/').rmdir()
 
-    def test_download_file_successful(self):
-        """Test a successful download."""
-        url = 'https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_VNM_0.json'
-        out_dir = Path('tests')
-        out_dir.mkdir(parents=True, exist_ok=True)
-        path = Path(out_dir, 'gadm41_VNM_0.json')
-        succeded = download_file(url, path)
-
-        # Check if the file has been created
-        expected = True
-        actual = path.exists()
-        self.assertEqual(expected, actual)
-
-        # Check if the value returned from the function is correct
-        expected = True
-        actual = succeded
-        self.assertEqual(expected, actual)
-
-        # Tear down
-        path.unlink()
-        Path('tests/').rmdir()
-
-    def test_download_file_unsuccessful(self):
-        """Test an unsuccessful download."""
-        # This will fail because a password is needed
-        url = 'http://aphrodite.st.hirosaki-u.ac.jp/product/' + \
-            'APHRO_V1808_TEMP/APHRO_MA/005deg/' + \
-            'APHRO_MA_TAVE_CLM_005deg_V1808.ctl.gz'
-        out_dir = Path('tests/product/APHRO_V1808_TEMP/APHRO_MA/005deg')
-        out_dir.mkdir(parents=True, exist_ok=True)
-        path = Path(out_dir, 'APHRO_MA_TAVE_CLM_005deg_V1808.ctl.gz')
-        succeded = download_file(url, path)
-
-        # Check if the file has been created
-        expected = False
-        actual = path.exists()
-        self.assertEqual(expected, actual)
-
-        # Check if the value returned from the function is correct
-        expected = False
-        actual = succeded
-        self.assertEqual(expected, actual)
-
-        # Tear down
-        Path('tests/product/APHRO_V1808_TEMP/APHRO_MA/005deg').rmdir()
-        Path('tests/product/APHRO_V1808_TEMP/APHRO_MA').rmdir()
-        Path('tests/product/APHRO_V1808_TEMP').rmdir()
-        Path('tests/product').rmdir()
-        Path('tests/').rmdir()
-
     def test_unpack_file(self):
         url = 'https://geodata.ucdavis.edu/gadm/gadm4.1/json/' + \
             'gadm41_VNM_1.json.zip'
@@ -192,17 +234,41 @@ class TestCases(unittest.TestCase):
         Path(out_dir, 'gadm41_VNM_1.json').unlink()
         Path('tests/').rmdir()
 
+    def test_download_geospatial_data(self):
+        data_name = 'GADM administrative map'
+        download_geospatial_data(data_name, only_one=False, dry_run=True)
+        self.test_download_gadm_admin_map_data()
+
+    def test_download_gadm_admin_map_data(self):
+        download_gadm_admin_map_data(only_one=False, dry_run=True)
+        base_dir = utils.get_base_directory()
+        root = Path(
+            base_dir, 'A Collate Data', 'Geospatial Data',
+            'GADM administrative map',
+        )
+        for branch in [
+            Path('gadm41_VNM.gpkg'),
+            Path('gadm41_VNM_shp.zip'),
+            Path('gadm41_VNM_0.json'),
+            Path('gadm41_VNM_1.json.zip'),
+            Path('gadm41_VNM_2.json.zip'),
+            Path('gadm41_VNM_3.json.zip'),
+        ]:
+            expected = True
+            actual = Path(root, branch).exists()
+            self.assertEqual(expected, actual)
+
     def test_download_meteorological_data(self):
         only_one = True
         dry_run = True
 
-        data_name = 'APHRODITE Daily mean temperature product (V1808)'
-        download_meteorological_data(data_name, only_one, dry_run)
-        self.test_download_aphrodite_temperature_data()
-
         data_name = 'APHRODITE Daily accumulated precipitation (V1901)'
         download_meteorological_data(data_name, only_one, dry_run)
         self.test_download_aphrodite_precipitation_data()
+
+        data_name = 'APHRODITE Daily mean temperature product (V1808)'
+        download_meteorological_data(data_name, only_one, dry_run)
+        self.test_download_aphrodite_temperature_data()
 
         data_name = 'CHIRPS: Rainfall Estimates from Rain Gauge and ' + \
             'Satellite Observations'
@@ -218,6 +284,25 @@ class TestCases(unittest.TestCase):
         download_meteorological_data(data_name, only_one, dry_run)
         self.test_download_era5_reanalysis_data()
 
+    def test_download_aphrodite_precipitation_data(self):
+        download_aphrodite_precipitation_data(only_one=True, dry_run=True)
+        base_dir = utils.get_base_directory()
+        root = Path(
+            base_dir, 'A Collate Data', 'Meteorological Data',
+            'APHRODITE Daily accumulated precipitation (V1901)',
+            'product', 'APHRO_V1901', 'APHRO_MA'
+        )
+        for branch in [
+            Path('005deg', 'APHRO_MA_PREC_CLM_005deg_V1901.ctl.gz'),
+            Path('025deg', 'APHRO_MA_025deg_V1901.2015.gz'),
+            Path('025deg_nc', 'APHRO_MA_025deg_V1901.2015.nc.gz'),
+            Path('050deg', 'APHRO_MA_050deg_V1901.2015.gz'),
+            Path('050deg_nc', 'APHRO_MA_050deg_V1901.2015.nc.gz'),
+        ]:
+            expected = True
+            actual = Path(root, branch).exists()
+            self.assertEqual(expected, actual)
+
     def test_download_aphrodite_temperature_data(self):
         download_aphrodite_temperature_data(only_one=True, dry_run=True)
         base_dir = utils.get_base_directory()
@@ -229,29 +314,10 @@ class TestCases(unittest.TestCase):
         for branch in [
             Path('005deg', 'APHRO_MA_TAVE_CLM_005deg_V1808.ctl.gz'),
             Path('005deg_nc', 'APHRO_MA_TAVE_CLM_005deg_V1808.nc.gz'),
-            Path('025deg', 'APHRO_MA_TAVE_025deg_V1808.1961.gz'),
-            Path('025deg_nc', 'APHRO_MA_TAVE_025deg_V1808.1961.nc.gz'),
+            Path('025deg', 'APHRO_MA_TAVE_025deg_V1808.2015.gz'),
+            Path('025deg_nc', 'APHRO_MA_TAVE_025deg_V1808.2015.nc.gz'),
             Path('050deg', 'read_aphro_v1808.f90'),
-            Path('050deg_nc', 'APHRO_MA_TAVE_050deg_V1808.1961.nc.gz'),
-        ]:
-            expected = True
-            actual = Path(root, branch).exists()
-            self.assertEqual(expected, actual)
-
-    def test_download_aphrodite_precipitation_data(self):
-        download_aphrodite_precipitation_data(only_one=True, dry_run=True)
-        base_dir = utils.get_base_directory()
-        root = Path(
-            base_dir, 'A Collate Data', 'Meteorological Data',
-            'APHRODITE Daily accumulated precipitation (V1901)',
-            'product', 'APHRO_V1901', 'APHRO_MA'
-        )
-        for branch in [
-            Path('005deg', 'APHRO_MA_PREC_CLM_005deg_V1901.ctl.gz'),
-            Path('025deg', 'APHRO_MA_025deg_V1901.1998.gz'),
-            Path('025deg_nc', 'APHRO_MA_025deg_V1901.1998.nc.gz'),
-            Path('050deg', 'APHRO_MA_050deg_V1901.1998.gz'),
-            Path('050deg_nc', 'APHRO_MA_050deg_V1901.1998.nc.gz'),
+            Path('050deg_nc', 'APHRO_MA_TAVE_050deg_V1808.2015.nc.gz'),
         ]:
             expected = True
             actual = Path(root, branch).exists()
@@ -274,7 +340,7 @@ class TestCases(unittest.TestCase):
             self.assertEqual(expected, actual)
 
     def test_download_terraclimate_data(self):
-        download_terraclimate_data(only_one=True, dry_run=True)
+        download_terraclimate_data(True, True, '2023')
         base_dir = utils.get_base_directory()
         root = Path(
             base_dir, 'A Collate Data', 'Meteorological Data',
@@ -282,7 +348,7 @@ class TestCases(unittest.TestCase):
             'TERRACLIMATE-DATA'
         )
         for branch in [
-            Path('fix_unsigned.sh'),
+            Path('TerraClimate_aet_2023.nc'),
         ]:
             expected = True
             actual = Path(root, branch).exists()
@@ -337,30 +403,6 @@ class TestCases(unittest.TestCase):
         )
         for branch in [
             Path('Viet_Nam_100m_Population.7z'),
-        ]:
-            expected = True
-            actual = Path(root, branch).exists()
-            self.assertEqual(expected, actual)
-
-    def test_download_geospatial_data(self):
-        data_name = 'GADM administrative map'
-        download_geospatial_data(data_name, only_one=False, dry_run=True)
-        self.test_download_gadm_admin_map_data()
-
-    def test_download_gadm_admin_map_data(self):
-        download_gadm_admin_map_data(only_one=False, dry_run=True)
-        base_dir = utils.get_base_directory()
-        root = Path(
-            base_dir, 'A Collate Data', 'Geospatial Data',
-            'GADM administrative map',
-        )
-        for branch in [
-            Path('gadm41_VNM.gpkg'),
-            Path('gadm41_VNM_shp.zip'),
-            Path('gadm41_VNM_0.json'),
-            Path('gadm41_VNM_1.json.zip'),
-            Path('gadm41_VNM_2.json.zip'),
-            Path('gadm41_VNM_3.json.zip'),
         ]:
             expected = True
             actual = Path(root, branch).exists()
