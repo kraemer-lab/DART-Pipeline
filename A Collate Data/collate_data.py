961b244062f594e5f4e395fcadf7464032542510
rowan@papa <rowan.charles.nicholls@gmail.com>
2024-05-08 23:03:53 +0100

    Finished testing: both test scripts in the A folder and both test scripts in the B folder

--- A Collate Data/collate_data.py
+++ A Collate Data/collate_data.py
@@ -681,6 +681,7 @@ def download_terraclimate_data(only_one, dry_run, year):
     - `time python3 collate_data.py "TerraClimate data"`:
         - 34:50.828
         - 11:35.25
+        - 14:16.896
     - `time python3 collate_data.py "TerraClimate data" -1 -d`: 00:04.606
     """
     data_type = 'Meteorological Data'
@@ -737,10 +738,10 @@ def download_worldpop_pop_density_data(only_one, dry_run):
 
     Run times:
 
-    - `time python3 collate_data.py "WorldPop pop density" -d`: 0m0.732s
+    - `time python3 collate_data.py "WorldPop pop density" -d`: 00:00.732
     - `time python3 collate_data.py "WorldPop pop density":
-        - 0m2.860s
-        - 0m0.29s
+        - 00:02.860
+        - 00:00.29
     """
     data_type = 'Socio-Demographic Data'
     data_name = 'WorldPop population density'
@@ -800,7 +801,9 @@ def download_worldpop_pop_count_data(only_one, dry_run):
 
     Run times:
 
-    - `time python3 collate_data.py "WorldPop pop count"`: 406.2s
+    - `time python3 collate_data.py "WorldPop pop count"`:
+        - 06:46.2
+        - 17:40.154
     """
     data_type = 'Socio-Demographic Data'
     data_name = 'WorldPop population count'
