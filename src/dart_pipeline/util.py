d6438b91203cf93b0b4ef41d8ca3a4cfb76786ba
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-08-25 18:55:31 +0100

    Simplify util, walk() returns list

--- src/dart_pipeline/util.py
+++ src/dart_pipeline/util.py
@@ -1,26 +1,23 @@
 """
 Utility library for DART pipeline
 """
-# External libraries
+
+import json
+import os
+import shutil
+import datetime
+import logging
+from datetime import timedelta
+from typing import Generator
+from pathlib import Path
+from urllib.parse import urljoin
+
 from lxml import html
 import gzip
 import py7zr
 import requests
 
-# Built-in modules
-from datetime import timedelta
-from pathlib import Path
-import json
-import os
-import shutil
-import datetime
-
-# Custom modules
-from typing import Generator
-from pathlib import Path
-# Create the requirements file from the terminal with:
-# $ python3 -m pip install pipreqs
-# $ pipreqs '.' --force
+from .types import Credentials
 
 
 def daterange(
@@ -31,9 +28,7 @@ def daterange(
         yield start_date + timedelta(n)
 
 
-def get_credentials(
-    metric: str, credentials: str | Path | None = None
-):
+def get_credentials(metric: str, credentials: str | Path | None = None):
     """
     Get a username and password pair from a credentials.json file.
 
@@ -85,7 +80,7 @@ def get_credentials(
 
     # fallback to file if no environment variable set
     credentials_path = (
-        Path('../..', "credentials.json") if credentials is None else Path(credentials)
+        Path("../..", "credentials.json") if credentials is None else Path(credentials)
     )
     if not credentials_path.exists():
         raise FileNotFoundError(
@@ -96,91 +91,47 @@ def get_credentials(
     return credentials_from_string(credentials_path.read_text(), metric)
 
 
-def download_file(
-    url: str, path: Path, username: str | None = None, password: str | None = None
-) -> bool:
+def download_file(url: str, path: Path, auth: Credentials | None = None) -> bool:
     """Download a file from a given URL to a given path."""
-    print("Downloading", url)
-    print("to", path)
-    # Make a request for the data
-    if username and password:
-        r = requests.get(url, auth=(username, password))
-    else:
-        r = requests.get(url)
-    # 401: Unauthorized
-    # 200: OK
-    if r.status_code == 200:
+    print("Downloading", url, "->", path)
+    if (r := requests.get(url, auth=auth)).status_code == 200:
         with open(path, "wb") as out:
             for bits in r.iter_content():
                 out.write(bits)
         return True
     else:
-        print("Failed with status code", r.status_code)
+        logging.error("Failed with status code", r.status_code)
         return False
 
 
 def download_files(
-    url: str,
-    files: list[str],
-    out_dir: Path,
-    only_one: bool = False,
-    username: str | None = None,
-    password: str | None = None,
+    url: str, files: list[str], out_dir: Path, auth: Credentials | None = None
 ) -> list[bool]:
     """Download multiple files in a list."""
-    successes = []
     # If the user requests it, only download the first file
-    if only_one:
-        files = files[:1]
-    # Download the files
-    for file in files:
-        # Create folder and intermediate folders
-        path = Path(out_dir, relative_url)
-        path.mkdir(parents=True, exist_ok=True)
-        # Get the file
-        file_url = base_url + "/" + relative_url + "/" + file
-        path = Path(path, file)
-        success = download_file(file_url, path, username, password)
-        successes.append(success)
-
-    return successes
+    Path(out_dir).mkdir(parents=True, exist_ok=True)
+    return [
+        download_file(urljoin(url, file), Path(out_dir, file), auth) for file in files
+    ]
 
 
 def walk(
-    url: str,
-    only_one: bool = False,
-    out_dir: Path = ".",
-    username: str | None = None,
-    password: str | None = None,
-):
+    url: str, out_dir: Path, auth: Credentials | None = None
+) -> list[tuple[str, list[str]]]:
     """
     Re-create `os.walk` and `Path.walk` for use with a website.
 
-    By default, all the files that are encountered by this walk function will
-    be downloaded into a matching file structure on the local machine.
+    Does not actually download the files, instead returns a list which
+    can be passed to download_files()
 
     Parameters
     ----------
-    base_url : str
+    url
         The base URL of the website being accessed.
-    relative_url : str
-        The base_url plus the relative_url points to the folder on the server
-        that will act as the starting point for the walk.
-    only_one : bool, default False
-        If True, only one file from each folder on the remote server will be
-        downloaded (if `dry_run=True` it will not be downloaded but an empty
-        file will created to represent it). This is to save on time and space
-        when testing functionality.
-    dry_run : bool, default False
-        If True, the data will be downloaded. If False, no data will be
-        downloaded but, instead, empty files will be created in the output
-        folder with a naming system matching that of a wet run.
-    out_dir : str, default '.'
+    out_dir
         The folder to which the data will be downloaded.
-    username : str, default None
-        The username associated with an account that can access the data.
-    password : str, default None
-        The password associated with an account that can access the data.
+    auth
+        Credentials associated with the walk if required
     """
     # In general, use strings for creating and handling URLs as opposed to
     # urllib's URL objects or path objects.
@@ -189,22 +140,17 @@ def walk(
     #   always use the urlunparse() function to convert back into a string
     # - Avoid os.path.join() because on Windows machines the slash will be the
     #   wrong way around
-    url = base_url + "/" + relative_url
-
     # We want to be able to identify the parent URL
     idx = url.rindex("/")
     parent_url = url[:idx]
 
-    if username and password:
-        page = requests.get(url, auth=(username, password))
-    else:
-        page = requests.get(url)
-    if page.status_code != 200:
-        print(f"Status code {page.status_code}")
+    if (page := requests.get(url, auth=auth)).status_code != 200:
+        logging.warning(f"Status code {page.status_code}")
     webpage = html.fromstring(page.content)
     links = webpage.xpath("//a/@href")
     # Classify the links on the page
     sorters = []
+    download_list = []
     children = []
     parents = []
     files = []
@@ -221,36 +167,26 @@ def walk(
             files.append(link)
     # Remove hidden files
     files = [x for x in files if not x.startswith(".")]
-
-    # Download the files
-    download_files(
-        base_url, relative_url, files, only_one, dry_run, out_dir, username, password
-    )
+    download_list = [(url, files)]
 
     for child in children:
-        relative_url_new = relative_url + "/" + child.removesuffix("/")
-        walk(base_url, relative_url_new, only_one, dry_run, out_dir, username, password)
+        url_new = url + "/" + child.removesuffix("/")
+        download_list += walk(url_new, out_dir, auth)
+    return download_list
 
 
-def unpack_file(path, same_folder=False):
+def unpack_file(path: Path | str, same_folder: bool = False):
     """Unpack a zipped file."""
     print("Unpacking", path)
-    if Path(path).suffix == ".7z":
-        if same_folder:
-            output_folder = Path(path).parent
-        else:
-            output_folder = str(path).removesuffix(".7z")
-        # Extract the 7z file
-        with py7zr.SevenZipFile(path, mode="r") as archive:
-            archive.extractall(output_folder)
-    elif Path(path).suffix == ".gz":
-        p_out = str(path).removesuffix(".gz")
-        with gzip.open(path, "rb") as f_in, open(p_out, "wb") as f_out:
-            f_out.write(f_in.read())
-    else:
-        if same_folder:
-            print("to", path.parent)
-            shutil.unpack_archive(path, path.parent)
-        else:
-            print("to", str(path).removesuffix(".zip"))
-            shutil.unpack_archive(path, str(path).removesuffix(".zip"))
+    path = Path(path)
+    match path.suffix:
+        case ".7z":
+            with py7zr.SevenZipFile(path, mode="r") as archive:
+                archive.extractall(path.parent if same_folder else path.stem)
+        case ".gz":
+            with gzip.open(path, "rb") as f_in, open(path.stem, "wb") as f_out:
+                f_out.write(f_in.read())
+        case _:
+            extract_dir = path.parent if same_folder else path.stem
+            print("to", extract_dir)
+            shutil.unpack_archive(path, extract_dir)
