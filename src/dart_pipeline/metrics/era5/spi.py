a9337d95e5639c28d67c5a50b8269000af64c2b2
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-03-23 12:04:13 +0000

    feat(era5): add standardised precipitation
    Co-authored-by: Iago Perez Fernandez <68069957+Winter-23@users.noreply.github.com>

---
+++ src/dart_pipeline/metrics/era5/spi.py
@@ -0,0 +1,101 @@
+"""
+Methods to calculate standardised precipitation index and
+standardised precipitation-evaporation index
+"""
+
+import functools
+import scipy.stats
+import numpy as np
+import xarray as xr
+
+from typing import Literal
+
+
+def adjust_gamma_distribution(
+    ds: xr.Dataset, window: int, dimension: str
+) -> tuple[float, float, float]:
+    ds_ma = ds.rolling(time=window, center=False).mean()
+    # Nat log of moving averages
+    ds_In = np.log(ds_ma)
+    ds_In = ds_In.where(not np.isinf(ds_In))
+    ds_mu = ds_ma.mean(dimension)
+
+    # Overall mean of moving averages
+    ds_mu = ds_ma.mean(dimension)
+
+    # Summation of Natural log of moving averages
+    ds_sum = ds_In.sum(dimension)
+
+    # Computing essentials for gamma distribution
+    n = ds_In[window - 1 :, :, :].count(dimension)  # size of dataset
+
+    A = np.log(ds_mu) - (ds_sum / n)
+    alpha = (1 / (4 * A)) * (1 + (1 + ((4 * A) / 3)) ** 0.5)
+    beta = ds_mu / alpha
+    return A, alpha, beta
+
+
+def standardized_precipitation(
+    var: Literal["spi", "spie", "bc_spi", "bc_spie"],
+    ds: xr.Dataset,
+    ds_ref: xr.Dataset,
+    window: int,
+    dimension: str,
+) -> xr.DataArray:
+    """
+    The goal of this function is to measure SPI or SPIE by introducing
+    precipitation/ precipitation - potential evapotranspiration data. In order
+    to measure SPI, first we need to measure the parameters of a gamma
+    distribution using historical data, and then adjust the precipitation data
+    to that distribution.
+
+    Parameters
+    ----------
+    ds
+       Data we want to adjust. Depending upon the choice of ``var``, certain
+       columns must be present. If var="SPI", ds and ds_ref must contain tp
+       (total_precipitation) if var="SPIE ds and ds_ref must contain the
+       product of precipitation - potential evapotranspiration
+    ds_ref
+       The historical dataset that we will use to obtain the gamma parameters
+    window
+       The time window length used to measure SPI.
+    dimension
+       The dimension over which to compute the rolling mean and gamma parameters
+    var
+       The variable name to be adjusted ('tp' for precipitation,
+       'tp_corrected' for corrected precipitation) 'balance' for measuring
+       SPIE
+
+    ds and ds_ref must contain the same variable (tp for measuring SPI and
+    precipitation - potential evapotranspiration if for SPIE)
+
+    Returns
+    -------
+    The standardized precipitation index (SPI) or SPIE for the given variable.
+    """
+    tp = "tp" if not var.startswith("bc_") else "bc_tp"
+    match var.removeprefix("bc_"):
+        case "spi":
+            A, alpha, beta = adjust_gamma_distribution(
+                ds_ref[tp], window=window, dimension=dimension
+            )
+        case "spie":
+            A, alpha, beta = adjust_gamma_distribution(
+                ds_ref, window=window, dimension=dimension
+            )
+    ds_ma = ds.rolling(time=window, center=False).mean(dim=dimension)
+
+    def gamma_func(data, a, scale):
+        return scipy.stats.gamma.cdf(data, a=a, scale=scale)
+
+    gamma = xr.apply_ufunc(gamma_func, ds_ma, alpha, beta)  # type: ignore
+    # standardized precipitation index (inverse of CDF)
+    norminv = functools.partial(scipy.stats.norm.ppf, loc=0, scale=1)
+    norm_spi = xr.apply_ufunc(norminv, gamma)
+
+    match var.removeprefix("bc_"):
+        case "spi":
+            return norm_spi[tp].rename(var)
+        case "spie":
+            return norm_spi.rename(var)
