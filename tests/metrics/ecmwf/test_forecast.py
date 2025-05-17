2c367b43b8cc635dc5d79a804328de89ffcc3929
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2025-05-17 22:37:14 +0100

    tests: add ecmwf.forecast tests

---
+++ tests/metrics/ecmwf/test_forecast.py
@@ -0,0 +1,82 @@
+"Tests for dart_pipeline.metrics.ecmwf forecast code"
+
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import pytest
+import freezegun
+
+from dart_pipeline.metrics.ecmwf import get_forecast_open_data
+
+
+@freezegun.freeze_time("2025-01-05")
+@patch("dart_pipeline.metrics.ecmwf.forecast_path")
+@patch("geoglue.country.Country")
+@patch("dart_pipeline.metrics.ecmwf.forecast_grib_to_netcdf")
+@patch("ecmwf.opendata.Client")
+@patch("dart_pipeline.paths.get_path")
+def test_successful_forecast_download(
+    mock_get_path,
+    mock_client_class,
+    mock_forecast_grib,
+    mock_country_class,
+    mock_forecast_path,
+):
+    # Setup test values
+    iso3 = "FRA"
+    date = "2025-01-03"
+    instant_ds = MagicMock()
+    accum_ds = MagicMock()
+
+    mock_forecast_path.return_value.exists.return_value = False
+
+    mock_country = MagicMock()
+    mock_country.bounds = MagicMock(north=50.0, south=40.0, west=-5.0, east=10.0)
+    mock_country_class.return_value = mock_country
+
+    mock_forecast_grib.return_value = (instant_ds, accum_ds)
+
+    sources_path = Path("/mock/sources")
+    mock_get_path.return_value = sources_path
+
+    result = get_forecast_open_data(iso3, date)
+
+    # Check result is a list of 2 files
+    assert len(result) == 2
+    assert result[0].name == "FRA-2025-01-03-ecmwf.forecast.instant.nc"
+    assert result[1].name == "FRA-2025-01-03-ecmwf.forecast.accum.nc"
+
+    # Check data saving was called
+    instant_ds.to_netcdf.assert_called_once()
+    accum_ds.to_netcdf.assert_called_once()
+
+    # Check retrieval was triggered
+    mock_client_class.return_value.retrieve.assert_called_once()
+
+
+@pytest.mark.parametrize("invalid_hour", [-1, 5, 24])
+def test_invalid_start_hour(invalid_hour):
+    with pytest.raises(ValueError, match="start_hour must be one of"):
+        get_forecast_open_data("FRA", "2025-01-05", start_hour=invalid_hour)
+
+
+@pytest.mark.parametrize("invalid_step", [0, 5, 7, 10])
+def test_invalid_step_hours(invalid_step):
+    with pytest.raises(ValueError, match="must be a multiple of 6"):
+        get_forecast_open_data("FRA", "2025-01-05", step_hours=invalid_step)
+
+
+@freezegun.freeze_time("2025-01-05")
+def test_future_date():
+    future_date = "2025-01-06"
+    with pytest.raises(ValueError, match="Can't fetch a forecast from the future"):
+        get_forecast_open_data("FRA", future_date)
+
+
+@freezegun.freeze_time("2025-01-05")
+def test_too_old_date():
+    old_date = "2024-12-31"  # 5 days in the past
+    with pytest.raises(
+        ValueError, match="Can't fetch a forecast more than 4 days in the past"
+    ):
+        get_forecast_open_data("FRA", old_date)
